ce289e0d03ebc8c0504493bb1c860074aa74d596
Ariel Alvarado <alvarado.ariel.contact@gmail.com>
2016-08-09 07:41:17 -0400

    first commit

---
+++ database/migrations/2016_08_08_004611_create_blog_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBlogTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('body');
+            $table->date('entry_date');
+            $table->date('published_date');
+            $table->integer('autor_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
